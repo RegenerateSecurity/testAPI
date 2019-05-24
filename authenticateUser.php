4480262df9e871cdad820bae9568eaaacfdf8c08
holly <holly@localhost.localdomain>
2019-05-24 05:45:48 -0400

    Writing unit tests

--- authenticateUser.php
+++ authenticateUser.php
@@ -8,22 +8,15 @@ $password = 'testy';
 $response = postJson('authenticateUser', '1.0', json_encode(array('username' => $email,'password' => $password)));
 $responseJson = json_decode($response, true);
 if ($responseJson === null) {
-  print 'API Error<br/>';
-  exit();
-}
-
-if (isset($responseJson['token'])) {
-  print "Token:" . $responseJson['token'] . '
-';
-$token = $responseJson['token'];
+  die('[F] Fatal error - API returned falafel instead of JSON' . PHP_EOL);
 }
 else if (isset($responseJson['error'])) {
-  print "Handled exception: " . $responseJson['error'] . '
-';
-  exit();
-} else {
-  print "Unhandled exception: " . $response . '
-';
-  exit();
+  die('[!] Error: ' . $responseJson['error'] . PHP_EOL);
+}
+else if (isset($responseJson['token'])) {
+  print '[+] Token: ' . $responseJson['token'] . PHP_EOL;
+}
+else {
+  die('[F] Fatal - received JSON but did not expect it' . PHP_EOL);
 }
 ?>
--- sessionCheck.php
+++ sessionCheck.php
@@ -5,11 +5,11 @@ $responseJson = json_decode($response, true);
 if ($responseJson === null) {
   print '[F] Fatal Error - API returned the bad daytas';
 }
-else if ($responseJson['error']) {
-  print '[!] ' . $responseJson['error'] . PHP_EOL;
+else if (isset($responseJson['error'])) {
+  print '[!] Error: ' . $responseJson['error'] . PHP_EOL;
 }
-else if ($responseJson['result']) {
-  print '[+] ' . $responseJson['result'] . PHP_EOL;
+else if (isset($responseJson['result'])) {
+  print '[+] Result: ' . $responseJson['result'] . PHP_EOL;
 }
 else {
   print '[F] Fatal Error - This should never happen. Got good JSON, but did not expect it';
